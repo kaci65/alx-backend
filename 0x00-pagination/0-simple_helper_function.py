8e1943742a92e19d186be6373a2d1b5b803f9e0a
Ubuntu <ubuntu@ip-172-31-7-225.us-east-2.compute.internal>
2021-08-09 19:23:44 +0000

    first commit

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+function named index_range that takes two integer arguments page and page_size
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    return a tuple of size two containing a start index and an end index
+    """
+    start = page * page_size - page_size
+    end = page * page_size
+    return (start, end)
