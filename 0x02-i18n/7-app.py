ee09f6313fbd3eb15727586b2560aba5f6c972e8
Ubuntu <ubuntu@ip-172-31-7-225.us-east-2.compute.internal>
2021-08-20 23:43:22 +0000

    First commit

---
+++ 0x02-i18n/7-app.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+"""
+setup a basic Flask app
+create a single / route and an index.html template
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, gettext
+from typing import Union
+from pytz import timezone
+import pytz.exceptions
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config(object):
+    """
+    create Config class that has LANGUAGES class attribute equal to
+    ["en", "fr"]
+    use Config to set Babel’s default locale ("en") and timezone ("UTC")
+    use that class as config for Flask app
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index() -> str:
+    """
+    return 7-index.html template
+    """
+    return render_template('7-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    determine the best match with our supported languages
+    """
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+
+    if g.user:
+        locale = g.user.get('locale')
+        if locale and locale in app.config['LANGUAGES']:
+            retun locale
+
+    langHeader = request.headers.get('Accept-Language')
+    if langHeader and langHeader in app.config['LANGUAGES']:
+        return langHeader
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user() -> Union[dict, None]:
+    """
+    returns a user dictionary or None if the ID cannot be found or if
+    login_as was not passed
+    """
+    user = request.args.get('login_as')
+    if user in users:
+        return users[int(user)]
+    else:
+        None
+
+
+@app.before_request
+def before_request():
+    """
+    use get_user to find a user if any, and set it as a global on flask.g.user
+    """
+    g.user = get_user()
+
+
+@babel.timezoneselector
+def get_timezone():
+    """
+    return URL-provided or user time zone
+    tz = timezone
+    """
+    tz = request.args.get('timezone')
+    if tz:
+        try:
+            return timezone(tz)
+        except UnknownTimeZoneError:
+            pass
+    if g.user:
+        user_tz = request.args.get('timezone')
+        if user_tz:
+            try:
+                return timezone(user_tz)
+            except UnknownTimeZoneError:
+                pass
+    return timezone(app.config['BABEL_DEFAULT_TIMEZONE'])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
